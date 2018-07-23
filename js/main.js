ad425089f85f5ddef86ee808712958998e8c3723
Angelica Harrison <harrison_angelica@yahoo.com>
2018-07-22 23:20:49 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-	console.log("User flipped " + cardOne);
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-	console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
+var cardOne = cards[0];
+	cardOne.push("cardsInPlay");
+	
+var cardTwo = cards[2];
+	cardTwo.push("cardsInPlay");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	} else {
+		alert("Sorry, try again");
+	}
+}
+
+
+
