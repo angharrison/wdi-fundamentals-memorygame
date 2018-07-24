28f185a003c49605527317403315688ca731adff
Angelica Harrison <harrison_angelica@yahoo.com>
2018-07-24 08:34:37 -0400

    unit 9

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-
-var cardOne = cards[0];
-	cardOne.push("cardsInPlay");
-	
-var cardTwo = cards[2];
-	cardTwo.push("cardsInPlay");
-
-if (cardsInPlay.length === 2) {
+function (checkForMatch) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	} else {
-		alert("Sorry, try again");
-	}
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
 
+var flipCard = function (cardId) {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1], alert("You found a match!") {
 
+	} else {
+			alert("Sorry, try again");
 
+			console.log("User flipped" + cards[cardId])
+		}
+		function checkForMatch; 
+	}
+	function flipCard(0);
+	function flipCard(2);
+}
